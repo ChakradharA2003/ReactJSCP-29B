aec574487f413d610f277d4a9d54ba0785fc730b
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-04-05 23:59:37 +0530

    Create index.js

---
+++ components/Home/index.js
@@ -0,0 +1,23 @@
+// Write your JS code here
+import {withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Home = props => {
+  const onClickedLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+  return (
+    <div className="home-container">
+      <Header />
+      <h1 className="home-heading">Home Route</h1>
+      <button type="button" className="logout-btn" onClick={onClickedLogout}>
+        Logout
+      </button>
+    </div>
+  )
+}
+export default withRouter(Home)
